38989c873326bbec4c2cfaa1d18f8c5d87df9707
Roope Marttinen <roope.marttinen@hotmail.com>
2019-08-03 13:39:43 +0300

    added toolbar, other minor improvements

--- app/src/main/java/com/example/roope/aopsmartpostapp/MainActivity.java
+++ app/src/main/java/com/example/roope/aopsmartpostapp/MainActivity.java
@@ -12,6 +12,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     TextView txtResOne, txtSeekBar;
     SeekBar seekBar;
     String stringErrorZip, stringErrorGeneral, stringLocationPermissionTitle, stringLocationPermissionMessage, stringNotFound, yes, no;
-    String stringLocationPermissionOK, stringLocationPermissionNO;
+    String stringLocationPermissionOK, stringLocationPermissionNO, stringNoPostis;
 
     private FusedLocationProviderClient mFusedLocationClient;
 
@@ -58,6 +61,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        txtEdit = findViewById(R.id.editSearch);
         btnSearch = findViewById(R.id.buttonSearch);
         btnLocate = findViewById(R.id.buttonLoc);
         btnShowOnMap = findViewById(R.id.buttonShowMap);
@@ -73,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
         stringNotFound = getResources().getString(R.string.locationNotFoundString);
         yes = getResources().getString(R.string.yesString);
         no = getResources().getString(R.string.noString);
+        stringNoPostis = getResources().getString(R.string.errorNoPostis);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
@@ -107,9 +115,7 @@ public class MainActivity extends AppCompatActivity {
         //nappi hakee syötteen perusteella
         btnSearch.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                txtEdit = findViewById(R.id.editSearch);
                 txtResOne.setText("");
-
                 String searchTerm = txtEdit.getText().toString();
                 String cleanedTerm = CheckInput(searchTerm);
                 //virhekäsittely zip koodille
@@ -136,6 +142,28 @@ public class MainActivity extends AppCompatActivity {
                 openMapByXY(lon, lat);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.emptyMenu) {
+            if (lonLatList != null && lonLatList.size() > 0) {
+                lonLatList.clear();
+            }
+            seekBar.setProgress(0);
+            txtSeekBar.setText("");
+            txtResOne.setText("");
+            txtEdit.setText("");
+        }else{
+            this.finishAndRemoveTask();
+        }
+        return true;
     }
 
     private void getLocation(){
@@ -313,19 +341,24 @@ public class MainActivity extends AppCompatActivity {
             //String mapQuery = "geo:0,0?z=12&q=";
             String mapQuery = "https://www.google.com/maps/dir/?api=1&destination=" + latitude + "," + longitude + "&waypoints=";
             sb.append(mapQuery);
-            for (Location loc : lonLatList){
-                String query = loc.getLatitude() + "," + loc.getLongitude();
-                sb.append(query);
-                sb.append("|");
-            }
-            sb.deleteCharAt(sb.length()-1);
-            String fullQuery = sb.toString();
-            String uri = fullQuery + "&travelmode=driving";
-            Intent mapIntent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
-            mapIntent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
-            startActivity(mapIntent);
-            if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            if (lonLatList.size() < 1){
+                Toast.makeText(MainActivity.this, stringNoPostis, Toast.LENGTH_LONG).show();
+
+            }else {
+                for (Location loc : lonLatList) {
+                    String query = loc.getLatitude() + "," + loc.getLongitude();
+                    sb.append(query);
+                    sb.append("|");
+                }
+                sb.deleteCharAt(sb.length() - 1);
+                String fullQuery = sb.toString();
+                String uri = fullQuery + "&travelmode=driving";
+                Intent mapIntent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
+                mapIntent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
                 startActivity(mapIntent);
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
             }
         } catch (Exception e){
             System.out.println("Error showing map");
